77adf0197e25e9bb62f327d690e96a7ee575ec46
AAlkandari <96235631+AAlkandari@users.noreply.github.com>
2021-12-27 10:22:10 +0300

    DONE

--- iterators.js
+++ iterators.js
@@ -5,9 +5,11 @@
  * - Logs every element of the array with "eng" next to it
  * eg: engineer(["zainab","omar","zahraa"]) => eng.zainab, eng.omar, eng.zahraa
  ************************************/
-const engineer = (array) => {
-  // input code here
-};
+  const array = ["zainab","omar","zahraa"];
+
+  const engineer = (array) => array.forEach(element => console.log("eng." + element));
+
+  engineer(array);
 
 /*************************************
  * Challenge 2: toKWD(wallets)
@@ -22,6 +24,12 @@ const engineer = (array) => {
  *   kwd = usd * 0.30
  ************************************/
 
+    const toKWD = (wallets) => (wallets.map((wallet) => wallet * 0.3));
+  
+    const kwdArray = toKWD([99,124,150]);
+
+    console.log(toKWD([99,124,150]));
+
 /**************************************
  * Challenge 3: richestWallet(wallets,maxAmount)
  *
@@ -31,6 +39,11 @@ const engineer = (array) => {
  * - Returns an array of wallets
  *   that exceed the maxAmount
  ***************************************/
+
+  const richestWallet = (wallets, maxAmount) => wallets.filter((wallet) => wallet > maxAmount);
+
+  console.log(richestWallet([90, 100, 200, 300, 400, 2333, 3222], 1000));
+
 
 /******************************************
  * Challenge 4: makeLiteralGrades(grades)
@@ -46,3 +59,20 @@ const engineer = (array) => {
  * < 60   : F
  * Example: makeLiteralGrades([90, 70, 40]) should return ["A", "C", "F"]
  *******************************************/
+
+const makeLiteralGrades = (grades) => {grades.map(element => {
+
+if (element >= 90) return "A";
+
+else if (element >= 80) return "B";
+
+else if (element >= 70) return "C";
+
+else if (element >= 60) return "D";
+
+else return "F";
+
+  })
+};
+
+console.log(makeLiteralGrades[90, 100, 70]);
